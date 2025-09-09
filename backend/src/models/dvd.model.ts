92e586e487e5095720c5587e605bd64c0a315c57
GDevWeb <zodiaquestar@gmail.com>
2025-09-09 13:18:39 +0200

    core(routes): created file routes - adding post logic

---
+++ backend/src/models/dvd.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IDVD extends Document {
+  eanCode: string;
+  title: string;
+  comments: string;
+  imageUrl?: string;
+  releaseYear?: number;
+  director?: string;
+}
+
+const dvdSchema: Schema = new Schema({
+  eanCode: { type: String, required: true, unique: true },
+  title: { type: String, required: true, unique: true },
+  comments: { type: String, required: false },
+  imageUrl: { type: String, required: false },
+  releaseYear: { type: String, required: false },
+  director: { type: String, required: false },
+});
+
+const DVD = mongoose.model<IDVD>("DVD", dvdSchema);
+
+export default DVD;
